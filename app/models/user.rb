bd09ce4a47c3f8edb14550d330d83deb07bd43e7
markolucic <marko_lucic_1994@yahoo.com>
2016-09-21 15:07:24 +0200

    added bootstrap sass

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
 	#valdiacija ostalih podataka (regex email), duzina passworda
 	EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
     validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
-  
+  	#before_save :encrypt_password
+    after_save :clear_password
+
+    def clear_password
+    	self.password = nil
+    end
 
 end
