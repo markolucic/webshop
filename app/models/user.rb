9909bd66bbb476a8df86f54183ce41fd92a37d3d
markolucic <marko_lucic_1994@yahoo.com>
2016-09-22 13:11:54 +0200

    changed model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-	before_create :confirmation_token
 	has_secure_password
 
 	#valdiacija ostalih podataka (regex email), duzina passworda
@@ -14,9 +13,4 @@ class User < ActiveRecord::Base
 
     private
 
-	def confirmation_token
-      if self.confirm_token.blank?
-          self.confirm_token = SecureRandom.urlsafe_base64.to_s
-      end
-    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: 'Marko', surname: 'Lucic', email: 'marko_lucic_1994@yahoo.com', phone: '066020297', is_admin: true, is_active: true, password: 'markolucic')
+User.create(name: 'Marko', surname: 'Lucic', email: 'marko_lucic_1994@yahoo.com', is_admin: true, is_active: true, password: 'markolucic')
