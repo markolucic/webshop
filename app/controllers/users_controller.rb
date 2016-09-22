5a140e51be4b5c92fe7ccb34a5933ffe392a7d5a
markolucic <marko_lucic_1994@yahoo.com>
2016-09-22 15:54:29 +0200

    added navbar and changed footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save			
-			UserMailer.registration_confirmation(@user).deliver
-        	flash[:success] = "Please confirm your email address to continue"
+        	flash[:success] = "You have successfully registered!"
 			redirect_to root_url
 		else
 			flash[:error] = "Ooooppss, something went wrong!"
@@ -20,12 +19,16 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :surname, :phone, :email, :is_admin, :is_active, :password_digest) #password_digest
+		params.require(:user).permit(:name, :surname, :email, :is_admin, :is_active, :password) #password_digest
 	end
 
 end
