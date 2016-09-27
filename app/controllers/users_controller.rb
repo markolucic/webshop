42099224feecd66a7fda7bf16db6dd196d92044f
markolucic <marko_lucic_1994@yahoo.com>
2016-09-27 12:02:50 +0200

    added edit user funcionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save			
         	flash[:success] = "You have successfully registered!"
-			redirect_to root_url
+			redirect_to login_path
 		else
 			render "new"
 		end
