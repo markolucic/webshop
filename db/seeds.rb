bc46468fd31928e093e0e8280c78e019fd3fc3c8
markolucic <marko_lucic_1994@yahoo.com>
2016-10-05 21:50:02 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.create(name: 'Marko', surname: 'Lucic', email: 'marko_lucic_1994@yahoo.com'
 	User.create!(name: name, surname: surname, email: email, password: password, 
 				 password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
-=end
+
 
 Category.create(name: "Muška obuća")
 Category.create(name: "Ženska obuća")
@@ -25,6 +25,23 @@ Category.create(name: "Dječija obuća")
 Category.create(name: "Sportska obuća")
 
 Product.create(name: "Starke", description: "Opis", price: 100, quantity: 10, image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
-Product.create(name: "Starke2", description: "Opis", price: 110, quantity: 10, image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
-Product.create(name: "Starke3", description: "Opis", price: 120, quantity: 10, image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
-Product.create(name: "Starke4", description: "Opis", price: 130, quantity: 10, image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
+Product.create(name: "Starke Converse", description: "Opis", price: 110, quantity: 10, image: "http://www.vijesti.me/media/cache/42/89/4289781ebf94f4b3d23e6231f62454e5.jpg")
+Product.create(name: "Vans Patike", description: "Opis", price: 120, quantity: 10, image: "http://images.vans.com/is/image/Vans/EE3BLK-HERO?$356x356$")
+Product.create(name: "Vans Duboke Patike", description: "Opis", price: 130, quantity: 10, image: "http://images.vans.com/is/image/Vans/XH8FH3-HERO?$356x356$")
+
+Color.create(name: "Crvena", hex: "ff0000")
+Color.create(name: "Zuta", hex: "ffff00")
+Color.create(name: "Zelena", hex: "33cc33")
+Color.create(name: "Plava", hex: "0000ff")
+Color.create(name: "Crna", hex: "000000")
+
+Brand.create(name: "Converse")
+Brand.create(name: "Nike")
+Brand.create(name: "Vans")
+Brand.create(name: "DC")
+Brand.create(name: "Toms")
+
+(38..46).each do |n|
+	Size.create(size: n)
+end
+=end
