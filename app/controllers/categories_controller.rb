96255d20ea66b448ad04dfa5853c239aa0e830bb
markolucic <marko_lucic_1994@yahoo.com>
2016-10-09 10:25:03 +0200

    categories views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,8 @@ class CategoriesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
+		#all products that have category_id equal to the searched one
+		@products = Product.joins("join categories_products on products.id = categories_products.product_id").where(["categories_products.category_id = ?", @category.id])
 	end
 
 	def new
