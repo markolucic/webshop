60e941ec9386fa9e8d7921669c8fc4645870abf7
markolucic <marko_lucic_1994@yahoo.com>
2016-10-10 13:35:34 +0200

    change seed and gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,6 @@ Category.create(name: "Ženska obuća")
 Category.create(name: "Dječija obuća")
 Category.create(name: "Sportska obuća")
 
-Product.create(name: "Starke", description: "Opis", price: 100, quantity: 10, image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
-Product.create(name: "Starke Converse", description: "Opis", price: 110, quantity: 10, image: "http://www.vijesti.me/media/cache/42/89/4289781ebf94f4b3d23e6231f62454e5.jpg")
-Product.create(name: "Vans Patike", description: "Opis", price: 120, quantity: 10, image: "http://images.vans.com/is/image/Vans/EE3BLK-HERO?$356x356$")
-Product.create(name: "Vans Duboke Patike", description: "Opis", price: 130, quantity: 10, image: "http://images.vans.com/is/image/Vans/XH8FH3-HERO?$356x356$")
-
 Color.create(name: "Crvena", hex: "ff0000")
 Color.create(name: "Zuta", hex: "ffff00")
 Color.create(name: "Zelena", hex: "33cc33")
@@ -42,42 +37,85 @@ Brand.create(name: "Vans")
 Brand.create(name: "DC")
 Brand.create(name: "Toms")
 
-(38..46).each do |n|
+(34..48).each do |n|
 	Size.create(size: n)
 end
 
+Product.create(name: "Starke", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ price: 100, quantity: 10, 
+ image: "http://www.24sata.rs/resources/images/0000/017/030/starke%20slika2_1000x0.jpg")
 
+Product.create(name: "Starke Converse", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+	price: 110, quantity: 10, 
+	image: "http://www.vijesti.me/media/cache/42/89/4289781ebf94f4b3d23e6231f62454e5.jpg")
+
+Product.create(name: "Vans Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+	price: 120, quantity: 10, 
+	image: "http://images.vans.com/is/image/Vans/EE3BLK-HERO?$356x356$")
+
+Product.create(name: "Vans Duboke Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+	price: 130, quantity: 10, 
+	image: "http://images.vans.com/is/image/Vans/XH8FH3-HERO?$356x356$")
 
 Product.create(name: "Vans Starke", 
 	description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 100, quantity: 10, 
-	image: "http://cdn.officeshoes.ws/product_images/2014pl/vuc6at6_blu.jpg?v1", brand_id: 1)
+	image: "http://cdn.officeshoes.ws/product_images/2014pl/vuc6at6_blu.jpg?v1")
 
 Product.create(name: "Starke Converse", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 110, quantity: 10, 
-	image: "http://brendovi.net/images/proizvodi-velike/Lacoste/lacos-spr12-obuca-musk2.jpg", brand_id: 1)
+	image: "http://brendovi.net/images/proizvodi-velike/Lacoste/lacos-spr12-obuca-musk2.jpg")
 
 Product.create(name: "Vans Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 120, quantity: 10, 
-	image: "http://cdn1.shopmania.biz/files/s3/396466939/p/l/1/platnene-muske-patike-m55105-teget~641.jpg", brand_id: 1)
+	image: "http://cdn1.shopmania.biz/files/s3/396466939/p/l/1/platnene-muske-patike-m55105-teget~641.jpg")
 
 Product.create(name: "Vans Duboke Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 130, quantity: 10, 
-	image: "http://cdn1.shopmania.biz/files/s3/396466939/p/l/3/platnene-muske-patike-m55208-teget~643.jpg", brand_id: 1)
+	image: "http://cdn1.shopmania.biz/files/s3/396466939/p/l/3/platnene-muske-patike-m55208-teget~643.jpg")
 
 Product.create(name: "Starke", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 100, quantity: 10, 
-	image: "http://www.buro247.hr/thumb/640x960_0/tenis7.jpg", brand_id: 2)
+	image: "http://www.buro247.hr/thumb/640x960_0/tenis7.jpg")
 
 Product.create(name: "Starke Converse", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 110, quantity: 10, 
-	image: "http://www.bravo.rs/wp-content/uploads/2013/04/VSCQ7FL.jpg", brand_id: 3)
+	image: "http://www.bravo.rs/wp-content/uploads/2013/04/VSCQ7FL.jpg")
 
 Product.create(name: "Vans Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 120, quantity: 10, 
-	image: "http://thefabweb.com/wp-content/uploads/2012/02/Vans-Valentines-Day-6.jpg", brand_id: 2)
+	image: "http://thefabweb.com/wp-content/uploads/2012/02/Vans-Valentines-Day-6.jpg")
 
 Product.create(name: "Vans Duboke Patike", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
 	price: 130, quantity: 10, 
-	image: "http://wannabemagazine.com/wp-content/uploads/2013/09/10086430_d1.jpg", brand_id: 4)
+	image: "http://wannabemagazine.com/wp-content/uploads/2013/09/10086430_d1.jpg")
+
+#random dodavanje brenda
+(1..12).each do |n|
+  p=Product.find(n)
+  p.brand=Brand.find(rand(1..5))
+  p.save
+end
+
+#dodavanje po 2 random kategorije
+(1..12).each do |n|
+  p=Product.find(n)
+  p.categories= [Category.find(rand(1..4)), Category.find(rand(1..4))]
+  p.save
+end
+
+#dodavanje po 3 random boje
+(1..12).each do |n|
+  p=Product.find(n)
+  p.colors= [Color.find(rand(1..5)), Color.find(rand(1..5)), Color.find(rand(1..5))]
+  p.save
+end
+
+#dodavanje po 5 velicina
+(1..12).each do |n|
+  p=Product.find(n)
+  p.sizes= [Size.find(rand(1..14)), Size.find(rand(1..14)), Size.find(rand(1..14)), Size.find(rand(1..14)), Size.find(rand(1..14))]
+  p.save
+end
+
 
