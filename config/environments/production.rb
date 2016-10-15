83271ff3e188d586d1bca130754f5a8785c025b7
markolucic <marko_lucic_1994@yahoo.com>
2016-10-15 22:39:34 +0200

    gmail smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,20 @@ Rails.application.configure do
     #user_name: ENV["GMAIL_USERNAME"],
     #password: ENV["GMAIL_PASSWORD"]
    #}
+  config.action_mailer.default_url_options = { :host => 'atlant-bh-shoes.herokuapp.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "atlant-bh-shoes.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
