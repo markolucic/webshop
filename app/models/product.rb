fdbce2fc95704f6228f4d5d33261a3371050ea6f
markolucic <marko_lucic_1994@yahoo.com>
2016-11-10 14:52:40 +0100

    s3 fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,11 +27,11 @@ class Product < ActiveRecord::Base
 
 
     def s3_credentials
-      {
-      	:bucket => ENV["S3_BUCKET"],
-	     :access_key_id => ENV["ACCESS_KEY_ID"],
-	     :secret_access_key => ENV["SECRET_ACCESS_KEY"]
-	  }
-	end
+    	{
+    		:bucket => ENV["S3_BUCKET"],
+		    :access_key_id => ENV["ACCESS_KEY_ID"],
+			:secret_access_key => ENV["SECRET_ACCESS_KEY"]
+    	}
+  	end
 
 end
