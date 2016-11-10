de3101706599153b1c4474c380508f2de1642af2
markolucic <marko_lucic_1994@yahoo.com>
2016-11-10 14:31:24 +0100

    s3 fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -26,11 +26,9 @@ class Product < ActiveRecord::Base
     :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
     def s3_credentials
-    {
       :bucket => ENV["S3_BUCKET"],
       :access_key_id => ENV["ACCESS_KEY_ID"],
       :secret_access_key => ENV["SECRET_ACCESS_KEY"]
-    }
 	end
-	
+
 end
