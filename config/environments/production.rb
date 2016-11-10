b99fbe7048b439e5bf90f7d6b5bf03d7573bb9ad
markolucic <marko_lucic_1994@yahoo.com>
2016-11-10 15:19:20 +0100

    s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
-  s3_region: ENV.fetch('AWS_REGION'),
+  s3_region: 'ireland',
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
